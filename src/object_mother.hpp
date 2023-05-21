3b5a29749e210e1975088bd37eceeb21f15991f5
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-05-21 21:57:16 +0200

    Add copyright notice to code

--- src/object_mother.hpp
+++ src/object_mother.hpp
@@ -1,3 +1,26 @@
+/*
+ ObjectMother creates objects for tests in a way object properties that are important.
+ Copyright (C) 2023  Fredrik Bülow
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ USA
+
+ Code is maintained here:
+ https://github.com/fbulow/object_mother/
+*/
+
 #pragma once
 #include <tuple>
 
